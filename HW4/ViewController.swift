a102cbd756b4bc5bea3ee2b88ac6ff4861a4f5ac
artemlapov <artemlapov@me.com>
2022-09-24 18:44:39 +0200

    Initial Commit

---
+++ HW4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW4
+//
+//  Created by Artem Lapov on 24.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
