e2601db2bfd93e3989197b9dfa3b57d656748459
artemlapov <artemlapov@me.com>
2022-09-25 14:42:58 +0200

    Интерфейс полностью готов, вместе с процентами слайдеров

--- HW4/ViewController.swift
+++ HW4/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var exampleOfColor: UIView!
+
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+
+    @IBOutlet var redPercentLabel: UILabel!
+    @IBOutlet var greenPercentLabel: UILabel!
+    @IBOutlet var bluePercentLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        exampleOfColor.layer.cornerRadius = exampleOfColor.frame.width / 15
+
     }
 
+    @IBAction func redSliderAction() {
+        redSlider.value = round(redSlider.value * 100) / 100
+        redPercentLabel.text = redSlider.value.formatted()
+    }
+
+    @IBAction func greenSliderAction() {
+        greenSlider.value = round(greenSlider.value * 100) / 100
+        greenPercentLabel.text = greenSlider.value.formatted()
+        exampleOfColor.backgroundColor = exampleOfColor.backgroundColor?.withAlphaComponent(CGFloat(greenSlider.value))
+    }
+
+    @IBAction func blueSliderAction() {
+        blueSlider.value = round(blueSlider.value * 100) / 100
+        bluePercentLabel.text = blueSlider.value.formatted()
+    }
 
 }
 
